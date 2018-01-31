aae7788d1de0ba3a78971451452e6b206dea6709
Alexander Hartson <alexander-hartson@live.com>
2018-01-30 23:35:56 -0600

    source code changes

--- Chapter 1/qt-tutorial-02/main.cpp
+++ Chapter 1/qt-tutorial-02/main.cpp
@@ -1,11 +1,11 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QLabel>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-
-    return a.exec();
+    QApplication app(argc, argv[]);
+    QLabel *label = new QLabel("Hello Qt!");
+    label->show();
+    return app.exec();
 }
