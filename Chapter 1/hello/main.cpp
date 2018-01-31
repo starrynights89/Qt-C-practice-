89cc2b4fc2df72523061223eb7858747afe4fe3e
Alexander Hartson <alexander-hartson@live.com>
2018-01-31 10:53:40 -0600

    style change

--- Chapter 1/hello/main.cpp
+++ Chapter 1/hello/main.cpp
@@ -5,7 +5,8 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QLabel *label = new QLabel("Hello Qt!");
+    QLabel *label = new QLabel("<h2><i>Hello</i>"
+                               "<font color=red>Qt!</font></h2>");
     label->show();
     return app.exec();
 }
