f1555b00d667909180fd4b5845e240f0ddd7f357
Alexander Hartson <alexander-hartson@live.com>
2018-01-31 11:43:44 -0600

    code change

--- Chapter 1/age/main.cpp
+++ Chapter 1/age/main.cpp
@@ -1,11 +1,33 @@
 #include "age_widget.h"
 #include <QApplication>
+#include <QHBoxLayout>
+#include <QSlider>
+#include <QSpingBox>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    age_widget w;
-    w.show();
+    QApplication app(argc, argv);
 
-    return a.exec();
+    QWidget *window = new QWidget;
+    window->setWindowTitle("Enter Your Age");
+
+    QSpinBox *spinBox = new QSpinBox;
+    QSlider *slider = new QSlider(Qt::Horizontal);
+    spinBox->setRange(0, 130);
+    slider->setRange(0, 130);
+
+    QObject::connect(spinBox, SIGNAL(valueChanged(int)),
+                     slider, SLOT(setValue(int)));
+    QObject::connect(slider, SIGNAL(valueChanged(int)),
+                     spinBox, SLOT(setValue(int)));
+    spinBox->setValue(35);
+
+    QHBoxLayout *layout = new QHBoxLayout;
+    layout->addWidget(spinBox);
+    layout->addWidget(slider);
+    window->setLayout(layout);
+
+    window->show();
+
+    return app.exec();
 }
