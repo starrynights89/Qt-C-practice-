21695f393e9e76ed32b391466e53b62724a272bd
Alexander Hartson <alexander-hartson@live.com>
2018-01-31 11:26:08 -0600

    changing code

--- Chapter 1/hello/main.cpp
+++ Chapter 1/hello/main.cpp
@@ -1,12 +1,14 @@
 #include "hello.h"
 #include <QApplication>
-#include <QLabel>
+#include <QPushButton>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QLabel *label = new QLabel("<h2><i>Hello</i> "
-                               "<font color=red>Qt!</font></h2>");
-    label->show();
+    QPushButton *button = new QPushButton("Quit");
+    //add constructor
+    QObject::connect(button, SIGNAL(clicked()),
+                     &app, SLOT(quit()));
+    button->show();
     return app.exec();
 }
