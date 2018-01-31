66b99c790eea80f089026618e8921c8fc7eb2419
Alexander Hartson <alexander-hartson@live.com>
2018-01-31 10:55:03 -0600

    code change

--- Chapter 1/hello/main.cpp
+++ Chapter 1/hello/main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QLabel *label = new QLabel("<h2><i>Hello</i>"
+    QLabel *label = new QLabel("<h2><i>Hello</i> "
                                "<font color=red>Qt!</font></h2>");
     label->show();
     return app.exec();
