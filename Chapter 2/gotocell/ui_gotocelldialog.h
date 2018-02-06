3d34163127eb0d6d30e1d0366688d80b63f63179
Alexander Hartson <alexander-hartson@live.com>
2018-02-06 16:06:32 -0600

    adding code

---
+++ Chapter 2/gotocell/ui_gotocelldialog.h
@@ -0,0 +1,13 @@
+class Ui::GoToCellDialog
+{
+puiblic:
+    QLabel *label;
+    QLineEdit *lineEdit;
+    QSpacerItem *spacerItem;
+    QPushButton *okButton;
+    QPushButton *cancelButton;
+     
+    void setupUi(QWidget *widget)
+    {
+    }
+};
