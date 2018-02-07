5c6c6e1e40abc7e4291968af3129d8309d9dae13
Alexander Hartson <alexander-hartson@live.com>
2018-02-07 17:16:04 -0600

    bugfix

--- Chapter 2/build-sort-Desktop_Qt_5_10_0_MinGW_32bit-Debug/ui_sortdialog.h
+++ Chapter 2/build-sort-Desktop_Qt_5_10_0_MinGW_32bit-Debug/ui_sortdialog.h
@@ -25,7 +25,7 @@
 
 QT_BEGIN_NAMESPACE
 
-class Ui_Sort
+class Ui_SortDialog
 {
 public:
     QGridLayout *gridLayout;
@@ -51,12 +51,12 @@ public:
     QLabel *label_8;
     QComboBox *comboBox_8;
 
-    void setupUi(QWidget *Sort)
+    void setupUi(QWidget *SortDialog)
     {
-        if (Sort->objectName().isEmpty())
-            Sort->setObjectName(QStringLiteral("Sort"));
-        Sort->resize(287, 298);
-        gridLayout = new QGridLayout(Sort);
+        if (SortDialog->objectName().isEmpty())
+            SortDialog->setObjectName(QStringLiteral("SortDialog"));
+        SortDialog->resize(287, 298);
+        gridLayout = new QGridLayout(SortDialog);
         gridLayout->setSpacing(6);
         gridLayout->setContentsMargins(11, 11, 11, 11);
         gridLayout->setObjectName(QStringLiteral("gridLayout"));
@@ -67,12 +67,12 @@ public:
         verticalLayout = new QVBoxLayout();
         verticalLayout->setSpacing(6);
         verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
-        okButton = new QPushButton(Sort);
+        okButton = new QPushButton(SortDialog);
         okButton->setObjectName(QStringLiteral("okButton"));
 
         verticalLayout->addWidget(okButton);
 
-        cancelButton = new QPushButton(Sort);
+        cancelButton = new QPushButton(SortDialog);
         cancelButton->setObjectName(QStringLiteral("cancelButton"));
 
         verticalLayout->addWidget(cancelButton);
@@ -81,7 +81,7 @@ public:
 
         verticalLayout->addItem(verticalSpacer);
 
-        moreButton = new QPushButton(Sort);
+        moreButton = new QPushButton(SortDialog);
         moreButton->setObjectName(QStringLiteral("moreButton"));
         moreButton->setCheckable(true);
 
@@ -90,7 +90,7 @@ public:
 
         gridLayout->addLayout(verticalLayout, 0, 1, 1, 1);
 
-        groupBox = new QGroupBox(Sort);
+        groupBox = new QGroupBox(SortDialog);
         groupBox->setObjectName(QStringLiteral("groupBox"));
         label = new QLabel(groupBox);
         label->setObjectName(QStringLiteral("label"));
@@ -110,7 +110,7 @@ public:
 
         gridLayout->addWidget(groupBox, 0, 0, 1, 1);
 
-        groupBox_3 = new QGroupBox(Sort);
+        groupBox_3 = new QGroupBox(SortDialog);
         groupBox_3->setObjectName(QStringLiteral("groupBox_3"));
         QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
         sizePolicy.setHorizontalStretch(0);
@@ -135,7 +135,7 @@ public:
 
         gridLayout->addWidget(groupBox_3, 4, 0, 1, 1);
 
-        groupBox_2 = new QGroupBox(Sort);
+        groupBox_2 = new QGroupBox(SortDialog);
         groupBox_2->setObjectName(QStringLiteral("groupBox_2"));
         QSizePolicy sizePolicy1(QSizePolicy::Preferred, QSizePolicy::Preferred);
         sizePolicy1.setHorizontalStretch(40);
@@ -169,51 +169,51 @@ public:
         QWidget::setTabOrder(okButton, cancelButton);
         QWidget::setTabOrder(cancelButton, moreButton);
 
-        retranslateUi(Sort);
-        QObject::connect(okButton, SIGNAL(clicked()), Sort, SLOT(accept()));
-        QObject::connect(cancelButton, SIGNAL(clicked()), Sort, SLOT(reject()));
+        retranslateUi(SortDialog);
+        QObject::connect(okButton, SIGNAL(clicked()), SortDialog, SLOT(accept()));
+        QObject::connect(cancelButton, SIGNAL(clicked()), SortDialog, SLOT(reject()));
         QObject::connect(moreButton, SIGNAL(toggled(bool)), groupBox_2, SLOT(setVisible(bool)));
         QObject::connect(moreButton, SIGNAL(toggled(bool)), groupBox_3, SLOT(setVisible(bool)));
 
-        QMetaObject::connectSlotsByName(Sort);
+        QMetaObject::connectSlotsByName(SortDialog);
     } // setupUi
 
-    void retranslateUi(QWidget *Sort)
+    void retranslateUi(QWidget *SortDialog)
     {
-        Sort->setWindowTitle(QApplication::translate("Sort", "Sort", nullptr));
-        okButton->setText(QApplication::translate("Sort", "OK", nullptr));
-        cancelButton->setText(QApplication::translate("Sort", "Cancel", nullptr));
-        moreButton->setText(QApplication::translate("Sort", "&More", nullptr));
-        groupBox->setTitle(QApplication::translate("Sort", "&Primary Key", nullptr));
-        label->setText(QApplication::translate("Sort", "Column:", nullptr));
-        comboBox->setItemText(0, QApplication::translate("Sort", "None", nullptr));
+        SortDialog->setWindowTitle(QApplication::translate("SortDialog", "Sort", nullptr));
+        okButton->setText(QApplication::translate("SortDialog", "OK", nullptr));
+        cancelButton->setText(QApplication::translate("SortDialog", "Cancel", nullptr));
+        moreButton->setText(QApplication::translate("SortDialog", "&More", nullptr));
+        groupBox->setTitle(QApplication::translate("SortDialog", "&Primary Key", nullptr));
+        label->setText(QApplication::translate("SortDialog", "Column:", nullptr));
+        comboBox->setItemText(0, QApplication::translate("SortDialog", "None", nullptr));
 
-        label_2->setText(QApplication::translate("Sort", "Order:", nullptr));
-        comboBox_2->setItemText(0, QApplication::translate("Sort", "Ascending ", nullptr));
-        comboBox_2->setItemText(1, QApplication::translate("Sort", "Descending", nullptr));
+        label_2->setText(QApplication::translate("SortDialog", "Order:", nullptr));
+        comboBox_2->setItemText(0, QApplication::translate("SortDialog", "Ascending ", nullptr));
+        comboBox_2->setItemText(1, QApplication::translate("SortDialog", "Descending", nullptr));
 
-        groupBox_3->setTitle(QApplication::translate("Sort", "&Tertiary Key", nullptr));
-        label_9->setText(QApplication::translate("Sort", "Column:", nullptr));
-        comboBox_9->setItemText(0, QApplication::translate("Sort", "None", nullptr));
+        groupBox_3->setTitle(QApplication::translate("SortDialog", "&Tertiary Key", nullptr));
+        label_9->setText(QApplication::translate("SortDialog", "Column:", nullptr));
+        comboBox_9->setItemText(0, QApplication::translate("SortDialog", "None", nullptr));
 
-        label_10->setText(QApplication::translate("Sort", "Order:", nullptr));
-        comboBox_10->setItemText(0, QApplication::translate("Sort", "Ascending ", nullptr));
-        comboBox_10->setItemText(1, QApplication::translate("Sort", "Descending", nullptr));
+        label_10->setText(QApplication::translate("SortDialog", "Order:", nullptr));
+        comboBox_10->setItemText(0, QApplication::translate("SortDialog", "Ascending ", nullptr));
+        comboBox_10->setItemText(1, QApplication::translate("SortDialog", "Descending", nullptr));
 
-        groupBox_2->setTitle(QApplication::translate("Sort", "&Secondary Key", nullptr));
-        label_7->setText(QApplication::translate("Sort", "Column:", nullptr));
-        comboBox_7->setItemText(0, QApplication::translate("Sort", "None", nullptr));
+        groupBox_2->setTitle(QApplication::translate("SortDialog", "&Secondary Key", nullptr));
+        label_7->setText(QApplication::translate("SortDialog", "Column:", nullptr));
+        comboBox_7->setItemText(0, QApplication::translate("SortDialog", "None", nullptr));
 
-        label_8->setText(QApplication::translate("Sort", "Order:", nullptr));
-        comboBox_8->setItemText(0, QApplication::translate("Sort", "Ascending ", nullptr));
-        comboBox_8->setItemText(1, QApplication::translate("Sort", "Descending", nullptr));
+        label_8->setText(QApplication::translate("SortDialog", "Order:", nullptr));
+        comboBox_8->setItemText(0, QApplication::translate("SortDialog", "Ascending ", nullptr));
+        comboBox_8->setItemText(1, QApplication::translate("SortDialog", "Descending", nullptr));
 
     } // retranslateUi
 
 };
 
 namespace Ui {
-    class Sort: public Ui_Sort {};
+    class SortDialog: public Ui_SortDialog {};
 } // namespace Ui
 
 QT_END_NAMESPACE
