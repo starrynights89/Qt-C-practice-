f90a74062dc5c82b1b68b199d8310facceac3e8d
Alexander Hartson <alexander-hartson@live.com>
2018-02-06 21:04:58 -0600

    code change

--- Chapter 2/gotocell/main.cpp
+++ Chapter 2/gotocell/main.cpp
@@ -6,10 +6,7 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    Ui::GoToCellDialog ui;
-    QDialog *dialog = new QDialog;
-    ui.setupUi(dialog);
+    GoToCellDialog *dialog = new GoToCellDialog;
     dialog->show();
-
     return app.exec();
 }
