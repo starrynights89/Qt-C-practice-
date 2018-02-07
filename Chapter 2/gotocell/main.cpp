455b35fdb676690530d37cf2f60dd7dc142ac9b4
Alexander Hartson <alexander-hartson@live.com>
2018-02-06 18:41:53 -0600

    adding code

--- Chapter 2/gotocell/main.cpp
+++ Chapter 2/gotocell/main.cpp
@@ -5,9 +5,12 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    gotocelldialog w;
-    w.show();
+    QApplication app(argc, argv);
 
-    return a.exec();
+    Ui:GoToCellDialog ui;
+    QDialog *dialog = new QDialog;
+    ui.setupUi(dialog);
+    dialog->show();
+   
+    return app.exec();
 }
