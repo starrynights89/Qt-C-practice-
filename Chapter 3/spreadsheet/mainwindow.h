4affcf27a31bcf4ff23e4531f26bc9969e1472fb
Alexander Hartson <alexander-hartson@live.com>
2018-02-09 12:31:14 -0600

    adding private classes

--- Chapter 3/spreadsheet/mainwindow.h
+++ Chapter 3/spreadsheet/mainwindow.h
@@ -27,6 +27,44 @@ private slots:
     void goToCell();
     void sort();
     void about();
+    void openRecentFile();
+    void updateStatusBar();
+    void spreadsheetModified();
+
+private:
+    void createActions();
+    void createMenus();
+    void createContextMenu();
+    void createToolBars();
+    void createStatusBar();
+    void readSettings();
+    void writeSettings();
+    bool okToContinue();
+    bool loadFile(const QString &fileName);
+    bool saveFile(const QString &fileName);
+    void setCurrentFile(const QString &fileName);
+    void updateRecentFileActions();
+    QString strippedName(const QString &fullFileName);
+    Spreadsheet *spreadsheet;
+    FindDialog *findDialog;
+    QLabel *locationLabel;
+    QLabel *formulaLabel;
+    QStringList recentFiles;
+    QString curFile;
+
+    enum { MaxRecentFiles = 5 };
+    QAction *recentFileActions[MaxRecentFiles];
+    QAction *separatorAction;
+
+    QMenu *fileMenu;
+    QMenu *editMenu;
+
+    QToolBar *fileToolBar;
+    QToolBar *editToolBar;
+    QAction *newAction;
+    QAction *openAction;
+
+    QAction *aboutQtAction;
 };
 
 #endif // MAINWINDOW_H
