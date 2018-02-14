943f8c8cd769a8b12205a0cb9cd49f5f789c1a58
Alexander Hartson <alexander-hartson@live.com>
2018-02-13 23:09:43 -0600

    code change

--- Chapter 3/spreadsheet/main.cpp
+++ Chapter 3/spreadsheet/main.cpp
@@ -3,9 +3,8 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    mainwindow w;
-    w.show();
-
-    return a.exec();
+    QApplication app(argc, argv);
+    MainWindow mainWin;
+    mainWin.show();
+    return app.exec();
 }
