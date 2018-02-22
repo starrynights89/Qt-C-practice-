b94296894c2583afef6c5dadb45f8e3e23740036
Alexander Hartson <alexander-hartson@live.com>
2018-02-21 19:29:58 -0600

    code changes

--- Chapter1/todo/mainwindow.cpp
+++ Chapter1/todo/mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->addTaskButton, &QPushButton::clicked,
+            this, &MainWindow::addTask);
     connect(ui->addTaskButton, &QPushButton::clicked,
             QApplication::instance(), &QApplication::quit);
 }
@@ -13,4 +17,9 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::addTask()
+{
+    qDebug() << "User clicked on the button!";
 }
