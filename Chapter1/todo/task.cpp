214c96808607b087f9c159fbc5a48c4b5cd48778
Alexander Hartson <alexander-hartson@live.com>
2018-02-21 21:33:24 -0600

    adding code

--- Chapter1/todo/task.cpp
+++ Chapter1/todo/task.cpp
@@ -6,9 +6,25 @@ Task::Task(QWidget *parent) :
     ui(new Ui::Task)
 {
     ui->setupUi(this);
+    setName(name);
 }
 
 Task::~Task()
 {
     delete ui;
+}
+
+void Task::setName(const QString &name)
+{
+    ui->checkbox->setText(name);
+}
+
+QString Task::name() const
+{
+    return ui->checkbox->text();
+}
+
+bool Task::isCompleted() const
+{
+    return ui->checkbox->isChecked();
 }
