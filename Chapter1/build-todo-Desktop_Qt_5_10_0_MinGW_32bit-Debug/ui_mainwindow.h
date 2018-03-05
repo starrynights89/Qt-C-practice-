945726fb195c6b581e38d6727b1bde6dc55ade1e
Alexander Hartson <alexander-hartson@live.com>
2018-03-05 15:56:58 -0600

    code changes

--- Chapter1/build-todo-Desktop_Qt_5_10_0_MinGW_32bit-Debug/ui_mainwindow.h
+++ Chapter1/build-todo-Desktop_Qt_5_10_0_MinGW_32bit-Debug/ui_mainwindow.h
@@ -13,11 +13,13 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QGridLayout>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QToolBar>
 #include <QtWidgets/QWidget>
@@ -28,7 +30,10 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralWidget;
+    QWidget *layoutWidget;
+    QGridLayout *gridLayout_2;
     QLabel *statusLabel;
+    QSpacerItem *horizontalSpacer;
     QPushButton *addTaskButton;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
@@ -38,19 +43,35 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
-        MainWindow->resize(400, 300);
+        MainWindow->resize(410, 292);
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
-        statusLabel = new QLabel(centralWidget);
+        layoutWidget = new QWidget(centralWidget);
+        layoutWidget->setObjectName(QStringLiteral("layoutWidget"));
+        layoutWidget->setGeometry(QRect(0, 0, 366, 25));
+        gridLayout_2 = new QGridLayout(layoutWidget);
+        gridLayout_2->setSpacing(6);
+        gridLayout_2->setContentsMargins(11, 11, 11, 11);
+        gridLayout_2->setObjectName(QStringLiteral("gridLayout_2"));
+        gridLayout_2->setContentsMargins(0, 0, 0, 0);
+        statusLabel = new QLabel(layoutWidget);
         statusLabel->setObjectName(QStringLiteral("statusLabel"));
-        statusLabel->setGeometry(QRect(10, 10, 111, 16));
-        addTaskButton = new QPushButton(centralWidget);
+
+        gridLayout_2->addWidget(statusLabel, 0, 0, 1, 1);
+
+        horizontalSpacer = new QSpacerItem(171, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        gridLayout_2->addItem(horizontalSpacer, 0, 1, 1, 1);
+
+        addTaskButton = new QPushButton(layoutWidget);
         addTaskButton->setObjectName(QStringLiteral("addTaskButton"));
-        addTaskButton->setGeometry(QRect(310, 10, 75, 23));
+
+        gridLayout_2->addWidget(addTaskButton, 0, 2, 1, 1);
+
         MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QStringLiteral("menuBar"));
-        menuBar->setGeometry(QRect(0, 0, 400, 21));
+        menuBar->setGeometry(QRect(0, 0, 410, 21));
         MainWindow->setMenuBar(menuBar);
         mainToolBar = new QToolBar(MainWindow);
         mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
@@ -67,7 +88,7 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
-        statusLabel->setText(QApplication::translate("MainWindow", "Status: 0 todo/0 done", nullptr));
+        statusLabel->setText(QApplication::translate("MainWindow", "Status: 0 todo / 0 done", nullptr));
         addTaskButton->setText(QApplication::translate("MainWindow", "Add task", nullptr));
     } // retranslateUi
 
